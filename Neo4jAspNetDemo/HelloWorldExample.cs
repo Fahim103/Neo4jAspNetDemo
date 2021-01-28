7cefce3698a52e73753759e03d7c877d3d140be8
Ahmed Ali Fahim <fahimahmedali1@gmail.com>
2021-01-28 14:54:35 +0600

    Getting Started with Neo4j

---
+++ Neo4jAspNetDemo/HelloWorldExample.cs
@@ -0,0 +1,46 @@
+﻿using Neo4j.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neo4jAspNetDemo
+{
+    public class HelloWorldExample : IDisposable
+    {
+        private readonly IDriver _driver;
+
+        public HelloWorldExample(string url, string userName, string password)
+        {
+            _driver = GraphDatabase.Driver(url, AuthTokens.Basic(userName, password));
+        }
+
+        public async Task PrintGreeting(string message)
+        {
+            var session = _driver.AsyncSession();
+
+            var greeting = await session.WriteTransactionAsync(async tx =>
+            {
+                var result = await tx.RunAsync(
+                    "CREATE (a:Greeting) SET a.message = $message RETURN a.message + ', from node ' + id(a)",
+                    new { message }
+                );
+
+                var data = await result.SingleAsync();
+
+                return data[0].As<string>();
+            });
+
+            Console.WriteLine(greeting);
+
+            await session.CloseAsync();
+            
+        }
+
+        public void Dispose()
+        {
+            _driver?.Dispose();
+        }
+    }
+}
--- Neo4jAspNetDemo/Program.cs
+++ Neo4jAspNetDemo/Program.cs
@@ -8,8 +8,11 @@ namespace Neo4jAspNetDemo
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            var hello = new HelloWorldExample("bolt://localhost:7687", "demo", "demo");
+            await hello.PrintGreeting("Hello Neo4j");
+            Console.ReadLine();
         }
     }
 }
